d7e3c8a89990ff472b69dbf2fc9e548d030dc5dd
syq <shenyongqiang2008@126.com>
2017-09-16 11:19:14 +0800

    添加新文件

---
+++ HardwareCheck/HardwareSliderDt.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class HardwareSliderDt : MonoBehaviour
+{
+    public int ZhouYunXingCmd;
+    public enum SliderEnum
+    {
+        YunXingShuLiang1,
+        YunXingShuLiang2,
+        YunXingShuLiang3,
+        YunXingShuLiang4,
+        YunXingSuDu1,
+        YunXingSuDu2,
+        YunXingSuDu3,
+        YunXingSuDu4,
+    }
+    public SliderEnum SliderState;
+}
